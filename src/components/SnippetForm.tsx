67277886b377180edcfa9111d927397139707b30
Gustavo Parra <gustavoparrabotero@gmail.com>
2023-05-11 12:34:49 -0400

    add snippet form

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { writeTextFile } from '@tauri-apps/api/fs'
+import { desktopDir } from '@tauri-apps/api/path'
+import { useState } from 'react';
 
 function SnippetForm() {
+  const [snippetName, setSnippetName] = useState('');
+
   return (
-    <div>SnippetForm</div>
+    <form onSubmit={async (e) => {
+      e.preventDefault();
+      const desktopPath = await desktopDir();
+      await writeTextFile(`${desktopPath}/snippets/${snippetName}.json`, `{}`);
+      setSnippetName('');
+    }}>
+      <input type='text'
+        placeholder='Write a Snippet'
+        className='bg-zinc-900 w-full border-none outline-none p-4'
+        onChange={(e) => setSnippetName(e.target.value)}
+        value={snippetName}
+      />
+      <button className='hidden'>
+        Save
+      </button>
+    </form>
   )
 }
 
